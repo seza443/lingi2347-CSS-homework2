94cfcbd775eb045c6fd97130ffd42acf0bee1993
Kagelmacher <alexandre.hauet@gmail.com>
2017-05-17 16:20:30 +0200

    build and run script set executable + creation and feed the table usersx

--- postgresql/SQL_dump/user.sql
+++ postgresql/SQL_dump/user.sql
@@ -1,1 +1,11 @@
-CREATE TABLE users ();
+CREATE TABLE usersx (
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+INSERT INTO usersx VALUES ('alex', 'azerty');
+INSERT INTO usersx VALUES ('tanguy', 'qsdfgh');
+INSERT INTO usersx VALUES ('max', 'T9yBBTHB');
+INSERT INTO usersx VALUES ('jean', 'dN3cJQcu');
+INSERT INTO usersx VALUES ('zazu', 'CxLeVRWz');
+INSERT INTO usersx VALUES ('nala', 'Y2Kgus7G');
+INSERT INTO usersx VALUES ('oreo', 'wfZB5XrH');
